0eee5fba6aa29c3001920c567e54dc5dd8deff08
Shafiqul Islam <www.shafiquebd@gmail.com>
2023-01-02 12:54:57 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:provider/provider.dart';
+import 'package:provider_api_call_example/provider/todo_provider.dart';
+import 'package:provider_api_call_example/screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<TodoProvider>(
+          create: (_) => TodoProvider(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // return ChangeNotifierProvider(
+    //   create: (context) => TodoProvider(),
+    //   child: const MaterialApp(
+    //     home: HomePage(),
+    //   ),
+    // );
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
